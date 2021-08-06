97d5dd6f71015ee5030bd21dd9908f8f28b2bf1b
Mithun Biswas <mithunbiswasbu92@gmail.com>
2021-08-06 16:42:35 +0600

    burj-al-arab client site done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD25bX0iG28gBkZAe3fmczH9cxNHdJOHps",
+    authDomain: "burj-al-arab-41761.firebaseapp.com",
+    projectId: "burj-al-arab-41761",
+    storageBucket: "burj-al-arab-41761.appspot.com",
+    messagingSenderId: "328711756488",
+    appId: "1:328711756488:web:6d4a3fae64ce83e76a63fd"
+  };
+
+export default firebaseConfig;
